9d94b671c938a07c19762dcec1fec9e38e67d844
oleg <zerex21@icloud.com>
2022-12-03 17:50:42 +0300

    feat: add ts and fix whitespace for eslint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey:'f81e012aaf054f8b9954be778dabd57e', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
