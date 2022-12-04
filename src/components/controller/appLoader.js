8a2203ea40401dc9bfadcf4b54748660ff1a1814
oleg <zerex21@icloud.com>
2022-12-04 15:04:23 +0300

    feat: rename index.ts and add ts

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey:'f81e012aaf054f8b9954be778dabd57e', // получите свой ключ https://newsapi.org/
+      apiKey: 'f81e012aaf054f8b9954be778dabd57e', // получите свой ключ https://newsapi.org/
     });
   }
 }
